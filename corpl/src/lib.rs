4b0ba0d511734995e33385d9dca560a65e47bbb5
Icelk <main@icelk.dev>
2021-02-11 19:59:46 +0100

    Added corpl program!

---
+++ corpl/src/lib.rs
@@ -0,0 +1,277 @@
+use std::{
+    fs::OpenOptions,
+    io::{Read, Seek, SeekFrom, Write},
+    path::Path,
+};
+
+use common::ExitDisplay;
+
+enum OptionEnabled {
+    Yes,
+    No,
+}
+
+enum Segment<'a> {
+    Section(&'a str),
+    Option(OptionEnabled),
+}
+
+pub fn process_file(path: &Path, comment: Option<&[u8]>, enabled: &[&str]) {
+    let mut file = match OpenOptions::new().read(true).write(true).open(path) {
+        Ok(f) => f,
+        Err(_) => "Failed to open config.".print_exit(),
+    };
+    let mut config = Vec::with_capacity(4096);
+    match file.read_to_end(&mut config) {
+        Err(_) => "Failed to read file.".print_exit(),
+        Ok(_) => {}
+    };
+    let line_ending = get_line_ending(&config);
+    let mut lines = get_lines(&config).peekable();
+    fn get_common_comments(bytes: &[u8]) -> Option<&'static [u8]> {
+        if bytes.starts_with(b"#") {
+            Some(b"#")
+        } else if bytes.starts_with(b"//") {
+            Some(b"//")
+        } else if bytes.starts_with(b";") {
+            Some(b";")
+        } else {
+            None
+        }
+    }
+    let comment = match get_common_comments(&config).or(comment) {
+        Some(c) => c,
+        None => {
+            let first_line = match lines.peek() {
+                Some(l) => *l,
+                None => "File too short; could not determine comment character.".print_exit(),
+            };
+            if first_line
+                .split(|b| is_whitespace(*b))
+                .next()
+                .unwrap()
+                .len()
+                <= 4
+            {
+                let comment = first_line.split(|b| b == &32).next().unwrap();
+                eprintln!(
+                    "Using uncommon comment: '{}'",
+                    String::from_utf8_lossy(comment)
+                );
+                comment
+            } else {
+                "Failed to get comment string. Please enter it, and only it, as the first line or supply the `-c` option with the comment string."
+                    .print_exit()
+            }
+        }
+    };
+
+    let mut in_section = false;
+    let mut in_option = false;
+    let mut section_common_string: &[u8] = b"";
+    let mut option_enabled = false;
+    let mut output = Vec::with_capacity(config.len() * 2);
+
+    for line in lines {
+        let line_trimmed = trim(line);
+
+        if line_trimmed.len() >= comment.len() + 1 + 5 + 1
+            && line_trimmed.starts_with(comment)
+            && line_trimmed[comment.len()..].starts_with(b" CORPL ")
+        {
+            let start = comment.len() + 1 + 5 + 1;
+
+            if line_trimmed[start..].starts_with(b"end") {
+                in_section = false;
+                in_option = false;
+            } else if line_trimmed[start..].starts_with(b"section ") {
+                let start = start + 8;
+                section_common_string = trim_last(&line_trimmed[start..]);
+
+                if section_common_string.is_empty() {
+                    eprintln!("Found a section with no replacement! Does no lines have anything in common, then append it to the section line and remove it from all the following.");
+                }
+
+                in_section = true;
+            } else if line_trimmed[start..].starts_with(b"option ") {
+                let start = start + 7;
+                let option = &line_trimmed[start..];
+                option_enabled = enabled.iter().any(|e| e.as_bytes() == option);
+                in_option = true;
+            }
+        } else if in_section {
+            let last = get_last(line, comment);
+            let activate = enabled.iter().any(|e| e.as_bytes() == last);
+            let start = first_non_whitespace(line);
+            let currently_active =
+                !(line[start..].starts_with(comment) && line[start + comment.len()] == 32);
+
+            if currently_active == activate {
+                // Do noting!
+            } else if !activate {
+                if line[start..].starts_with(section_common_string) {
+                    // Do stuff
+                    output.extend_from_slice(&line[..start]);
+                    output.extend_from_slice(comment);
+                    output.push(32);
+                    output.extend_from_slice(&line[start + section_common_string.len()..]);
+                    output.extend_from_slice(line_ending);
+                    continue;
+                } else {
+                    eprintln!("Common string of section not present! Ignoring line.")
+                }
+            } else if activate {
+                output.extend_from_slice(&line[..start]);
+                output.extend_from_slice(section_common_string);
+                output.extend_from_slice(&line[start + comment.len() + 1..]);
+                output.extend_from_slice(line_ending);
+                continue;
+            }
+        } else if in_option {
+            let start = first_non_whitespace(line);
+            let currently_active =
+                !(line[start..].starts_with(comment) && line[start + comment.len()] == 32);
+
+            if currently_active == option_enabled {
+                // Do nothing
+            } else if !option_enabled {
+                output.extend_from_slice(&line[..start]);
+                output.extend_from_slice(comment);
+                output.push(32);
+                output.extend_from_slice(&line[start..]);
+                output.extend_from_slice(line_ending);
+                continue;
+            } else if option_enabled {
+                output.extend_from_slice(&line[..start]);
+                output.extend_from_slice(&line[start + comment.len() + 1..]);
+                output.extend_from_slice(line_ending);
+                continue;
+            }
+        }
+        output.extend_from_slice(line);
+        // Newline character
+        output.extend_from_slice(line_ending);
+    }
+    match file.set_len(output.len() as u64) {
+        Ok(_) => {}
+        Err(_) => "Failed to set file length.".print_exit(),
+    };
+    match file.seek(SeekFrom::Start(0)) {
+        Ok(_) => {}
+        Err(_) => "Failed to seek in file.".print_exit(),
+    }
+    match file.write_all(&output[..]) {
+        Ok(_) => {}
+        Err(_) => "Failed to write to file.".print_exit(),
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Lines<'a> {
+    bytes: &'a [u8],
+    current_pos: usize,
+}
+impl<'a> Iterator for Lines<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_pos == self.bytes.len() {
+            return None;
+        }
+        let mut end = None;
+        for (pos, byte) in self.bytes[self.current_pos..].iter().enumerate() {
+            if *byte == 13 || *byte == 10 {
+                end = Some(pos);
+                break;
+            }
+        }
+        let end = match end {
+            Some(e) => e + self.current_pos,
+            None => self.bytes.len(),
+        };
+        let bytes = &self.bytes[self.current_pos..end];
+        let new_start = {
+            if self.bytes[end] == 13 {
+                end + 2
+            } else {
+                end + 1
+            }
+        };
+        self.current_pos = new_start;
+        Some(bytes)
+    }
+}
+fn get_lines(bytes: &[u8]) -> Lines {
+    Lines {
+        bytes,
+        current_pos: 0,
+    }
+}
+
+fn is_whitespace(byte: u8) -> bool {
+    match byte {
+        32 | 9..=13 => true,
+        _ => false,
+    }
+}
+fn trim(bytes: &[u8]) -> &[u8] {
+    let mut s = 0;
+    let mut e = bytes.len();
+
+    for (pos, byte) in bytes.iter().enumerate() {
+        if !is_whitespace(*byte) {
+            s = pos;
+            break;
+        }
+    }
+    for (pos, byte) in bytes.iter().enumerate().rev() {
+        if !is_whitespace(*byte) {
+            e = pos + 1;
+            break;
+        }
+    }
+    &bytes[s..e]
+}
+fn trim_last(bytes: &[u8]) -> &[u8] {
+    let mut e = 0;
+
+    for (pos, byte) in bytes.iter().enumerate().rev() {
+        if !is_whitespace(*byte) {
+            e = pos + 1;
+            break;
+        }
+    }
+
+    &bytes[..e]
+}
+fn first_non_whitespace(bytes: &[u8]) -> usize {
+    let mut s = 0;
+
+    for (pos, byte) in bytes.iter().enumerate() {
+        if !is_whitespace(*byte) {
+            s = pos;
+            break;
+        }
+    }
+    s
+}
+
+fn get_last<'a>(bytes: &'a [u8], to_match: &[u8]) -> &'a [u8] {
+    for (pos, bytes_sub) in bytes.windows(to_match.len() + 1).enumerate().rev() {
+        if &bytes_sub[..to_match.len()] == to_match && bytes_sub[to_match.len()] == 32 {
+            return &bytes[pos + to_match.len() + 1..];
+        }
+    }
+    return &bytes[0..0];
+}
+
+fn get_line_ending(bytes: &[u8]) -> &'static [u8] {
+    for byte in bytes.iter().copied() {
+        match byte {
+            13 => return b"\r\n",
+            10 => return b"\n",
+            _ => {}
+        }
+    }
+    b"\n"
+}
---
+++ corpl/src/main.rs
@@ -0,0 +1,55 @@
+use std::path::Path;
+
+use clap::{self, App, Arg};
+
+mod lib;
+
+fn main() {
+    let app = App::new("corpl")
+        .version("0.1.0")
+        .author("Icelk <main@icelk.dev>")
+        .about("Changes exposed values in config files")
+        .long_about("Changes exposed values in config files.\n\
+Tries to find the appropriate comment string (e.g. '#' and '//') in the first line. \
+A good practise for the first line to only contain the comment string.")
+        .arg(
+            Arg::with_name("CONFIG")
+                .help("Sets the config files to change. It is recommended to only use one config file per instance of this program, since the `-c` option overrides all unrecognised comment strings.")
+                .required(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("enabled")
+            .help("Which section to enable. Can be multiple.")
+                .short("e")
+                .long("enabled")
+                .takes_value(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("long-comment")
+            .help("Enables long comments. Must be used if comment string in file is greater than 4 bytes")
+                .short("l")
+                .long("long-comment"),
+        )
+        .arg(
+            Arg::with_name("c").help("Override comment string found in file. Can be used if the program failed to register it.")
+                .short("c")
+                .long("comment-string")
+                .takes_value(true),
+        );
+    let matches = app.get_matches();
+
+    let enabled: Vec<&str> = match matches.values_of("enabled") {
+        Some(enabled) => enabled.collect(),
+        None => Vec::new(),
+    };
+
+    for file in matches.values_of("CONFIG").unwrap() {
+        lib::process_file(
+            Path::new(file),
+            matches.value_of("c").map(str::as_bytes),
+            &enabled,
+        );
+    }
+}
