7f369d712c708f7e9bbcb26c321f1ecc0b6d0604
Icelk <main@icelk.dev>
2021-12-02 20:45:13 +0100

    Added split_split function in common. Functions as str::split but for slices.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -80,3 +80,80 @@ pub fn confirm(message: &str, default: Option<bool>) -> bool {
         println!("Failed to get intent. Type 'y' or 'n'{}", default);
     }
 }
+
+pub enum SliceSplitPredicateResult {
+    Continue,
+    Match {
+        /// The count of elements to ignore after the match.
+        length: usize,
+    },
+}
+pub trait SliceSplitPredicate<T> {
+    fn matches(&mut self, rest: &[T]) -> SliceSplitPredicateResult;
+}
+impl<T: PartialEq> SliceSplitPredicate<T> for &[T] {
+    fn matches(&mut self, rest: &[T]) -> SliceSplitPredicateResult {
+        if rest.starts_with(self) {
+            SliceSplitPredicateResult::Match { length: self.len() }
+        } else {
+            SliceSplitPredicateResult::Continue
+        }
+    }
+}
+impl<T: PartialEq, const C: usize> SliceSplitPredicate<T> for &[T; C] {
+    fn matches(&mut self, rest: &[T]) -> SliceSplitPredicateResult {
+        let mut slice = &self[..];
+        slice.matches(rest)
+    }
+}
+impl<T, F: FnMut(&[T]) -> SliceSplitPredicateResult> SliceSplitPredicate<T> for F {
+    fn matches(&mut self, rest: &[T]) -> SliceSplitPredicateResult {
+        self(rest)
+    }
+}
+pub fn slice_split<T, P: SliceSplitPredicate<T>>(
+    slice: &[T],
+    predicate: P,
+) -> SliceSplit<'_, T, P> {
+    SliceSplit {
+        index: 0,
+        last_split: 0,
+        predicate,
+        slice,
+    }
+}
+pub struct SliceSplit<'a, T, Predicate: SliceSplitPredicate<T>> {
+    index: usize,
+    last_split: usize,
+    predicate: Predicate,
+    slice: &'a [T],
+}
+impl<'a, T, Predicate: SliceSplitPredicate<T>> Iterator for SliceSplit<'a, T, Predicate> {
+    type Item = &'a [T];
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.index + 1 >= self.slice.len() {
+                if self.last_split < self.slice.len() {
+                    let slice = &self.slice[self.last_split..];
+
+                    self.last_split = self.slice.len();
+
+                    return Some(slice)
+                }
+                return None;
+            }
+            if self.last_split <= self.index {
+                match self.predicate.matches(&self.slice[self.index..]) {
+                    SliceSplitPredicateResult::Match { length } => {
+                        let slice = &self.slice[self.last_split..self.index + 1];
+
+                        self.last_split += length;
+                        return Some(slice);
+                    }
+                    SliceSplitPredicateResult::Continue => {}
+                }
+            }
+            self.index += 1;
+        }
+    }
+}
